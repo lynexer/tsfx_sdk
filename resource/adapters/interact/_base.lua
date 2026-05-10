35546636e4bf89d53b2719a34fc1b76aaa52e410
Kyle Graham <kylegraham05@gmail.com>
2026-05-10 09:28:20 -0700

    feat: add basic interaction adapters

--- resource/adapters/interact/_base.lua
+++ resource/adapters/interact/_base.lua
@@ -1,9 +1,24 @@
 --[[
-    TSFX SDK - Interaction Adapter Base
-
+    TSFX SDK - Interact Adapter Base
     Interface contract that all interaction adapters must implement.
 --]]
 
 ---@class InteractAdapterClass : IInteract
 InteractAdapterClass = {}
 InteractAdapterClass.__index = InteractAdapterClass
+
+function InteractAdapterClass:addBoxZone(params)
+    error('InteractAdapterClass:addBoxZone not implemented')
+end
+
+function InteractAdapterClass:addSphereZone(params)
+    error('InteractAdapterClass:addSphereZone not implemented')
+end
+
+function InteractAdapterClass:addEntityZone(entity, params)
+    error('InteractAdapterClass:addEntityZone not implemented')
+end
+
+function InteractAdapterClass:removeZone(name)
+    error('InteractAdapterClass:removeZone not implemented')
+end
--- resource/adapters/interact/_custom.lua
+++ resource/adapters/interact/_custom.lua
@@ -1,9 +1,24 @@
 --[[
-    TSFX SDK - Custom Interaction Adapter
-
+    TSFX SDK - Custom Interact Adapter
     Fallback when no interaction resource is detected. Logs warnings for all calls.
 --]]
 
 ---@class CustomInteractAdapter : InteractAdapterClass
 CustomInteractAdapter = setmetatable({}, { __index = InteractAdapterClass })
 CustomInteractAdapter.__index = CustomInteractAdapter
+
+function CustomInteractAdapter:addBoxZone(params)
+    _TSFX.Log:warn('CustomInteractAdapter:addBoxZone called but no interaction system is configured')
+end
+
+function CustomInteractAdapter:addSphereZone(params)
+    _TSFX.Log:warn('CustomInteractAdapter:addSphereZone called but no interaction system is configured')
+end
+
+function CustomInteractAdapter:addEntityZone(entity, params)
+    _TSFX.Log:warn('CustomInteractAdapter:addEntityZone called but no interaction system is configured')
+end
+
+function CustomInteractAdapter:removeZone(name)
+    _TSFX.Log:warn('CustomInteractAdapter:removeZone called but no interaction system is configured')
+end
